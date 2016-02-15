2c71a11826367fb86ca7ceb39ac13489a8505929
ned rihine <ned.rihine@gmail.com>
2016-02-15 21:21:50 +0900

    correct so number of time test gose wrong may be return

--- test/01-simple.scm
+++ test/01-simple.scm
@@ -6,10 +6,17 @@
 
 (with-output-to-file "cat.txt"
   (lambda ()
-    (cat "/usr/share/guile/2.0/ice-9/boot-9.scm")))
+    (cat ;"/usr/share/guile/2.0/ice-9/boot-9.scm"
+         "/usr/share/guile/2.0/srfi/srfi-64/testing.scm")))
 
 (with-input-from-file "cat.txt"
   (lambda ()
-    (test-assert (not (eof-object? (read-line))))))
+    (test-assert
+     (not (eof-object? (read-line))))))
+
+(test-equal 2 (* 1 1))
 
 (test-end "01-simple")
+
+(let ((runner (test-runner-current)))
+  (exit (test-runner-fail-count runner)))
